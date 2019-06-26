e109e10afdcafa187512d240c100b5198b1b85e1
MOHAMMAD PASHA <nag.chennamadhava123@gmail.com>
2019-06-26 15:30:28 +0530

    Initial commit

---
+++ src/main/java/S.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class S {
+    public static void main(String[] args) {
+
+        String baseUrl = "https://www.myntra.com/";
+        String driverPath = "C:\\Users\\mohammadp\\Desktop\\Drivers\\chromedriver.exe";
+        String Webdriver = "webdriver.chrome.driver";
+
+        System.setProperty(Webdriver, driverPath);
+        WebDriver driver  = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+
+    }
+
+}
+
